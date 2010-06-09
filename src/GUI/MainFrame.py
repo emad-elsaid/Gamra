3593c289bc1bdb97f6e9b5038a02b828698a9af1
Emad Elsaid <blazeeboy@gmail.com>
2010-06-09 17:45:33 +0300

    canvas and tollbar added to the main frame

--- src/GUI/MainFrame.py
+++ src/GUI/MainFrame.py
@@ -5,6 +5,7 @@ Created on Jun 4, 2010
 '''
 import wx
 import os
+import Canvas
 from AboutBox import AboutBox
 
 class MainFrame(wx.Frame):
@@ -14,10 +15,16 @@ class MainFrame(wx.Frame):
         
         wx.Frame.__init__(self, parent, id, title,pos, size)
         
+        #============= Creating statusbar
         self.CreateStatusBar()
+        
+        #============= Creating About Box
         self.About = AboutBox()
+        
+        #============= Setting Frame Icon
         self.Icon = wx.Icon(os.path.normpath("icons/icon256.png"), wx.BITMAP_TYPE_PNG)
-
+        
+        #============= Making Main Menu
         file_menu = wx.Menu()
         file_menu.Append(wx.ID_EXIT,"E&xit", "Terminate the program")
 
@@ -28,11 +35,35 @@ class MainFrame(wx.Frame):
         menuBar.Append(file_menu, "&File")
         menuBar.Append(help_menu, "&Help")
         self.MenuBar = menuBar
-                    
+        
+        #=================================================
+        #======= Making Main layout of the window ========
+        #=================================================
+        box = wx.BoxSizer(wx.VERTICAL)
+        self.SetAutoLayout(True)
+        self.SetSizer(box)
+        self.Layout()
+        #=============== Creating Canvas =================
+        canvas = Canvas.Canvas( self, size=wx.Size(1000,1000) )  
+        box.Add(canvas, 20, wx.EXPAND)     
+        #=============== Creating Toolbar =================
+        
+        tb = wx.ToolBar(self)
+        tb.height = 10
+        tb.AddTool(-1, wx.Bitmap("icons/select.png"), wx.Bitmap("icons/select.png"))
+        box.Add(tb, 2)
+        
+        #====== TODO: the properties table
+        panel2 = wx.Panel(self)
+        panel2.SetBackgroundColour("RED")
+        box.Add(panel2, 10, wx.EXPAND)
+        
+        
+
         #connecting functions with actions
         wx.EVT_MENU(self, wx.ID_EXIT, self.OnExit)
         wx.EVT_MENU(self, wx.ID_ABOUT, self.OnAbout)
-        
+                
     def OnExit(self, event):
         self.Close(True)
         
