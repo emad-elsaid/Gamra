ed2daf524e0e1bc38162daa0f80b25c1d68ba300
R4md4c <mr.ahmed_i@live.com>
2010-06-17 01:28:16 +0800

    The ColourPickerButton Not yet finished

--- src/GUI/ColourPicker/ColourPickerButton.py
+++ src/GUI/ColourPicker/ColourPickerButton.py
@@ -4,26 +4,27 @@ Created on Jun 16, 2010
 @author: ramdac
 '''
 import wx
-import os.path
+
  
 class ColourPickerButton:
     def __init__(self, parent, id, label = wx.EmptyString,pos = wx.DefaultPosition,  size = wx.DefaultSize, 
-         style = 0, validator = wx.DefaultValidator, name = "button"        ):
+         style = 0, validator = wx.DefaultValidator, name = "button"):
         
         self.button = wx.Button(parent, id, label, pos, size, style, validator, name)
         self.button.Show(False)
         
-        self.paletteImage = wx.Image("data/asd/colorcube.gif")
-        self.bmpImage = self.paletteImage.ConvertToBitmap()
         
-        self.panel = wx.Panel(parent, wx.ID_ANY, self.button.GetPosition(), (self.bmpImage.GetWidth(), self.bmpImage.GetHeight()), wx.TAB_TRAVERSAL)
+        self.bmpImage = wx.Bitmap("../../data/palettes/colorcube.gif", wx.BITMAP_TYPE_GIF)
+        
+        self.panel = wx.Panel(parent, wx.ID_ANY, self.button.GetPosition(), (self.bmpImage.GetWidth(), self.bmpImage.GetHeight()),  wx.TAB_TRAVERSAL)
+        wx.WindowDC(parent).DrawBitmap(self.bmpImage, 0, 0)
         self.panel.Show(False)
+        
         
         self.button.Bind(wx.EVT_BUTTON, self.OnClick)
     
     
     def Show(self, flag=True):
-        
         self.button.Show(flag)
     
     def OnClick(self, event):
@@ -36,10 +37,9 @@ class ColourPickerButton:
         
 if __name__ == "__main__":
     A = wx.App()
-    frame = wx.Frame(None)
+    frame = wx.Frame(None, pos = (500, 400))
     frame.Show(True)
-    a = ColourPickerButton(frame, -1, 'Hello')
-    
+    a = ColourPickerButton(frame, -1, 'Hello', pos = (100, 100))
     a.Show(True)
     A.MainLoop()
     
