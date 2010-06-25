9de210982b62bc37c950cb6cc3a83faff49337f6
Ahmed Ibrahim Khalil <ramdac@ramdac.(none)>
2010-06-26 03:33:39 +0800

    Finalized the Generic Panel class and ready to be inherited from

--- src/GUI/Editor/Generic.py
+++ src/GUI/Editor/Generic.py
@@ -11,27 +11,19 @@ class Generic(wx.Panel):
     '''
     
 
-    def __init__(self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.DefaultSize, iconPath='../../data/icons/photo.png', Priority=0 ):
+    def __init__(self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.DefaultSize, iconPath='data/icons/photo.png', Priority=0 ):
         wx.Panel.__init__(self, parent, id, pos, size)
         self.Priority = Priority
         self.icon = wx.Bitmap(iconPath, wx.BITMAP_TYPE_PNG)
         staticBitmap = wx.StaticBitmap(self, wx.ID_ANY, self.icon)
         self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.leftSizer = wx.BoxSizer(wx.VERTICAL)
-        self.rightSizer = wx.BoxSizer(wx.VERTICAL)
         self.leftSizer.Add(staticBitmap, 0, wx.ALIGN_TOP | wx.ALIGN_CENTER)
         self.mainSizer.Add(self.leftSizer, 0)
-        self.mainSizer.Add(self.rightSizer, 0)
         self.mainSizer.SetSizeHints(self)
         self.SetSizer(self.mainSizer)
-        self.Show(True)
+        self.Show(False)
         
     def Activate(self, canvas ): pass
     def Deactivate(self, canvas): pass
         
-if __name__ == "__main__":
-    a = wx.App()
-    frame = wx.Frame(None, wx.ID_ANY, "Hello")
-    frame.Show(True)
-    x = Generic(frame, wx.ID_ANY)
-    a.MainLoop()
