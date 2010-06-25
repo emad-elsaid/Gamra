8257043a46a40bb5d214e37661ab417dce9fa293
Ahmed Ibrahim Khalil <ramdac@ramdac.(none)>
2010-06-25 22:19:53 +0300

    Implemented the Generic Panel which will be the base for all panels in this program

--- src/GUI/Editor/Generic.py
+++ src/GUI/Editor/Generic.py
@@ -3,17 +3,35 @@ Created on Jun 23, 2010
 
 @author: blaze
 '''
-
-class Generic:
+import wx
+class Generic(wx.Panel):
     '''
-    this is the generic editor , all editors will enherit it's
+    this is the generic editor , all editors will inherit it's
     functionality
     '''
     
 
-    def __init__(self, icon='data/icons/photo.png', Periority=0 ):
-        self.Periority = Periority
+    def __init__(self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.DefaultSize, iconPath='../../data/icons/photo.png', Priority=0 ):
+        wx.Panel.__init__(self, parent, id, pos, size)
+        self.Priority = Priority
+        self.icon = wx.Bitmap(iconPath, wx.BITMAP_TYPE_PNG)
+        staticBitmap = wx.StaticBitmap(self, wx.ID_ANY, self.icon)
+        self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.leftSizer = wx.BoxSizer(wx.VERTICAL)
+        self.rightSizer = wx.BoxSizer(wx.VERTICAL)
+        self.leftSizer.Add(staticBitmap, 0, wx.ALIGN_TOP | wx.ALIGN_CENTER)
+        self.mainSizer.Add(self.leftSizer, 0)
+        self.mainSizer.Add(self.rightSizer, 0)
+        self.mainSizer.SetSizeHints(self)
+        self.SetSizer(self.mainSizer)
+        self.Show(True)
         
     def Activate(self, canvas ): pass
     def Deactivate(self, canvas): pass
         
+if __name__ == "__main__":
+    a = wx.App()
+    frame = wx.Frame(None, wx.ID_ANY, "Hello")
+    frame.Show(True)
+    x = Generic(frame, wx.ID_ANY)
+    a.MainLoop()
