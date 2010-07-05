1ce058c7f11d17076121dd74d821509abaaff581
ahmeds <ahmeds@ahmed>
2010-07-06 05:01:28 +0800

    i refresh panel properties

--- src/GUI/ToolBar/Tools.py
+++ src/GUI/ToolBar/Tools.py
@@ -60,7 +60,8 @@ class Tool():
         
     def OnMouseRightDown(self,event): event.Skip()
     
-    def OnMouseLeftUp(self,event): event.Skip()
+    def OnMouseLeftUp(self,event): 
+        wx.GetApp().Frame.Properties.Refresh(wx.GetApp().Frame.Canvas)
     
     def OnMouseMiddleUp(self,event): event.Skip()
         
