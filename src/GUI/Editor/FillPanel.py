636d17ce9c4a29a5202f0925757acc8adcb89754
Emad Elsaid <blazeeboy@gmail.com>
2010-07-05 21:29:06 +0300

    FillPanel created

---
+++ src/GUI/Editor/FillPanel.py
@@ -0,0 +1,27 @@
+'''
+Created on Jul 5, 2010
+
+@author: blaze
+'''
+import wx
+from GUI.Editor.Generic import Generic
+from GUI.ColourPicker import ColourPickerWidget
+
+class FillPanel(Generic):
+    def __init__(self,parent):
+        Generic.__init__(self, parent, iconPath="data/icons/fill.png")
+                
+        vertical = wx.BoxSizer(wx.VERTICAL)
+        
+        firstLine = wx.BoxSizer(wx.HORIZONTAL)
+        vertical.Add(firstLine)
+        
+        self.color = ColourPickerWidget.ColourPickerWidget(self)
+        firstLine.Add(self.color)
+        
+        firstLine.Add(wx.StaticText(self,-1,"Thickness :"))
+        
+        self.widthCtrl = wx.TextCtrl(self,-1,'1')
+        firstLine.Add(self.widthCtrl)
+        
+        self.mainSizer.Add(vertical)
