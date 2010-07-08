5bab3ca3b23c54ed32d0f415dce24f9733f22dd5
ahmeds <ahmeds@ahmed>
2010-07-09 04:26:19 +0800

    Stroke required changes made

--- src/GUI/Editor/StrokePanel.py
+++ src/GUI/Editor/StrokePanel.py
@@ -18,10 +18,21 @@ class StrokePanel(Generic):
         
         self.color = ColourPickerWidget.ColourPickerWidget(self)
         firstLine.Add(self.color)
+       
         
         firstLine.Add(wx.StaticText(self,-1,"Thickness :"))
         
         self.widthCtrl = wx.TextCtrl(self,-1,'1')
         firstLine.Add(self.widthCtrl)
+        self.mainSizer.AddSpacer(wx.Size(5, 5))
+        
         
         self.mainSizer.Add(vertical)
+        self.mainSizer.AddSpacer(wx.Size(10, 10))
+    def Activate(self,canvas):
+        if len(canvas.Document.SelectedObjects)==1 :
+            self.Show()
+            self.widthCtrl.SetValue("%s" %(canvas.Document.SelectedObjects[0].Stroke.Width))
+            self.color.SetColour(canvas.Document.SelectedObjects[0].Stroke.Color)
+        else:
+            self.Hide()
