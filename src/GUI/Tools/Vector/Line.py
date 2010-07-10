a8c51659f01f83314381ba390732e4bf0aa35823
ahmed <ahmed@ahmed.(none)>
2010-07-10 09:06:56 +0300

    line tool added

---
+++ src/GUI/Tools/Vector/Line.py
@@ -0,0 +1,31 @@
+from GUI.Tools.Tools import VectorTool
+
+import Document
+
+class Line(VectorTool):
+    '''
+    this tool for mdraw line the selected objects
+    '''
+    def __init__ (self):
+        VectorTool.__init__(self,name='Line',icon='line.png')
+    def OnMouseLeftDown(self,event):
+        
+        self.StartPoint = self.Canvas.Document.Mouse
+        obj = Document.Object()
+        obj.Path.add1(self.StartPoint[0],self.StartPoint[1])
+        obj.Path.add1(self.StartPoint[0],self.StartPoint[1])
+        
+        self.Canvas.Document.Objects.append(obj)
+        self.Canvas.Refresh()
+        
+        
+    def OnMouseMove(self,event):
+        if event.Dragging() and event.LeftIsDown() :
+            obj = self.Canvas.Document.Objects[-1]
+            obj.Path.Points[1][1][0] = self.Canvas.Document.Mouse[0]
+            obj.Path.Points[1][1][1] = self.Canvas.Document.Mouse[1]
+            self.Canvas.Refresh()
+            
+        VectorTool.OnMouseMove(self,event)
+	
+	
